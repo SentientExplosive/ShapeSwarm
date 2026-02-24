94e105f7cc05834956ea4c900dca0701798407b7
SentientExplosive <tnt.beep@gmail.com>
2026-02-24 14:14:08 -0500

    Arrow Test File

---
+++ senseTesting/arrowTest.py
@@ -0,0 +1,25 @@
+from sense_hat import SenseHat
+import time
+sense = SenseHat()
+
+b = (0,0,0) # Black
+a = (255,255,255) # arrow color
+
+# Array for just the arrow
+arrow_pixels = [
+    b, b, b, a, a, b, b, b,
+    b, b, b, a, a, b, b, b,
+    b, b, b, a, a, b, b, b,
+    b, b, b, a, a, b, b, b,
+    b, b, b, a, a, b, b, b,
+    b, b, b, a, a, b, b, b,
+    a, a, a, a, a, a, a, a, 
+    a, a, a, a, a, a, a, a
+]
+
+# Display arrow array on sense hat pixels
+sense.set_pixels(arrow_pixels)
+time.sleep(5)
+
+# Clear pixels
+sense.clear()
