d36ddd169fb84755d97cf89934f47c92e777e9d2
SentientExplosive <tnt.beep@gmail.com>
2026-05-05 18:24:52 -0400

    Fix for sauron launch file

--- sauron_ros_code/launch.py
+++ sauron_ros_code/launch.py
@@ -5,11 +5,20 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription([
         # Vision
-        Node(package='vision', node_executable='allSeeingEye', output='screen'),
+        Node(package='vision_pkg',
+             namespace='allSeeingEye',
+             executable='allSeeingEye',
+             output='screen'),
 
         # comms
-        Node(package='comms', node_executable='ringWraithPub', output='screen'),
-        Node(package='comms', node_executable='ringWraithSub', output='screen')
+        Node(package='comms',
+             namespace='ringWraithPub',
+             executable='ringWraithPub',
+             output='screen'),
+        Node(package='comms',
+             namespace='ringWraithSub',
+             executable='ringWraithSub',
+             output='screen')
 
     ])
     return ld
