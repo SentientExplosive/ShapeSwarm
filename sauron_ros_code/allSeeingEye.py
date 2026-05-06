9a59056dcd8439808a7405015e96a256778a8357
SentientExplosive <tnt.beep@gmail.com>
2026-05-06 13:57:02 -0400

    Please work now

--- sauron_ros_code/allSeeingEye.py
+++ sauron_ros_code/allSeeingEye.py
@@ -23,7 +23,7 @@ class allSeeingEye(Node):
         self.count = 0
 
         # Camera capture
-        self.cap = cv2.VideoCapture("/dev/video0")
+        self.cap = cv2.VideoCapture("/dev/video0", cv2.CAP_V4L2)
         if not self.cap.isOpened():
             print("Camera failed to open")
         # Set resolution (optional, like -r 1280x720)
